a9395ad253f52017f5974c1b3cef864a1d9ec982
Gurpreet Randhawa <singhpreet01991@gmail.com>
2022-11-20 14:44:35 -0800

    add updated image path to serve images from server

--- routes/videos.js
+++ routes/videos.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const path = require("node:path");
 const videosJSONFile = path.join(__dirname, "../data/videos.json");
 const videos = require(videosJSONFile);
+const PORT = process.env.PORT;
 
 /**
  * Get all the videos with required keys
@@ -14,7 +15,7 @@ router.get("/", (_req, res) => {
       return {
         id: video.id,
         title: video.title,
-        image: video.image,
+        image: `http://localhost:${PORT}/images/` + video.image,
         channel: video.channel,
       };
     });
@@ -30,7 +31,10 @@ router.get("/", (_req, res) => {
 router.get("/:id", (req, res) => {
   const found = videos.some((video) => video.id === req.params.id);
   if (found) {
-    res.json(videos.filter((video) => video.id === req.params.id));
+    // res.json(videos.filter((video) => video.id === req.params.id));
+    const main_video = videos.filter((video) => video.id === req.params.id);
+    const main_video_obj = main_video[0];
+    res.json(main_video_obj);
   } else {
     res.status(400).json({ error: `Video with ID:${req.params.id} not found` });
   }
