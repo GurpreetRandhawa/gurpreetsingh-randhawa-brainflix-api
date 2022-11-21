0a245cac273adcc141e0e5c56b1347a90a7322d8
Gurpreet Randhawa <singhpreet01991@gmail.com>
2022-11-20 20:26:56 -0800

    change upload post data

---
+++ helper/helper.js
@@ -0,0 +1,23 @@
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
+
+function writeJSONFile(filename, content) {
+  fs.writeFileSync(filename, JSON.stringify(content), "utf8", (err) => {
+    if (err) {
+    }
+  });
+}
+
+const getNewId = () => {
+  return uuidv4();
+};
+
+const timestamp = () => {
+  return Date.now();
+};
+
+module.exports = {
+  writeJSONFile,
+  getNewId,
+  timestamp,
+};
